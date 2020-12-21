d0af8cbdc5a4aeb91b5bcbef68c8f173f2b0363a
Sami Koivu <sami.koivu@gmail.com>
2020-12-21 12:24:28 +0200

    Fix saving just one frame over and over on replay

--- scripts/zombies.js
+++ scripts/zombies.js
@@ -303,7 +303,7 @@ _testbed('Car', function(testbed) {
 					quality: 10
 				  });
 				for (var i=0; i < 240; i++) {
-					  gif.addFrame(frames[frameStart]);
+					  gif.addFrame(frames[frameStart++]);
 					if (frameStart > 240) {
 						frameStart = 0;
 					}
